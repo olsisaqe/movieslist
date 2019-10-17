0883aa426954cd5969496ead6075ca514f56fc1e
Olsi Saqe <olsi_saqe@yahoo.it>
2019-10-17 17:55:53 +0200

    Creates the native ios app.

---
+++ native/MovieList/MovieList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieList
+//
+//  Created by Olsi Saqe on 17.10.19.
+//  Copyright © 2019 Olsi Saqe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
